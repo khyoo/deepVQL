24a7a978f1602aca2e30a31364d4c5cf1cd7d116
khyoo1221 <khyoo1221@gmail.com>
2023-01-13 13:46:51 +0900

    detail 수정

--- deepVQL_demo/controllers/indexController.js
+++ deepVQL_demo/controllers/indexController.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 
 module.exports = {
 	index: function (req, res, next) {
-		res.render('index', { title: 'deepVQL Demo' });
+		res.render('index', { title: 'DeepVQL Demo' });
 	},
 	demo: function (req, res, next) {
-		res.render('demo', { title: 'deepVQL Demo' });
+		res.render('demo', { title: 'DeepVQL Demo' });
 	},
 	// down1Create: function (req, res, next) {
 	// 	res.send({result:'ok'});
